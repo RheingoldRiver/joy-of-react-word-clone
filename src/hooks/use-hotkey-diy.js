a84d9592765159c0ca422fa039c778918cce58e5
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2023-02-08 07:40:35 -0600

    add other file

---
+++ src/hooks/use-hotkey-diy.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import useModifierKey from "./use-modifier-key";
+
+function useHotkeyDIY(modifierKey, letterKey, action) {
+  const modifierKeyDown = useModifierKey(modifierKey);
+  useEffect(() => {
+    function doActionOnKeypress(e) {
+      if (!modifierKeyDown) return;
+      if (e.isComposing || e.keyCode === 229) return;
+      if (e.key !== letterKey.toLowerCase()) return;
+      action();
+    }
+
+    window.addEventListener("keydown", doActionOnKeypress);
+    return () => {
+      window.removeEventListener("keydown", doActionOnKeypress);
+    };
+  }, [modifierKeyDown, letterKey, action]);
+}
+
+export default useHotkeyDIY;
